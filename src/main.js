0f388e762b0f62b2d0a007cd8e509304b1d56040
GiovanyMX <86213786+GiovanyMX@users.noreply.github.com>
2023-11-23 00:01:19 -0600

    Add files via upload

--- src/main.js
+++ src/main.js
@@ -9,11 +9,11 @@ const render = Matter.Render.create({
 render.canvas.width = 1000;
 render.canvas.height = 600;
 
-let containerLeft = Matter.Bodies.rectangle(-5, 300, 100, (render.canvas.height), { isStatic: true });
+let containerLeft = Matter.Bodies.rectangle(-50, 300, 100, (render.canvas.height), { isStatic: true });
 
-let containerRight = Matter.Bodies.rectangle((render.canvas.width -5), 300, 10, (render.canvas.height), { isStatic: true });
+let containerRight = Matter.Bodies.rectangle((render.canvas.width +50), 300, 100, (render.canvas.height), { isStatic: true });
 
-let containerDown = Matter.Bodies.rectangle(500, (render.canvas.height -5), (render.canvas.width), 10, { isStatic: true });
+let containerDown = Matter.Bodies.rectangle(500, (render.canvas.height +50), (render.canvas.width + 200), 100, { isStatic: true });
 
 let mouse = Matter.Mouse.create(render.canvas);
 let mouseConstraint = Matter.MouseConstraint.create(engine, {
@@ -25,8 +25,8 @@ let mouseConstraint = Matter.MouseConstraint.create(engine, {
 
 render.mouse = mouse;
 
-let stack = Matter.Composites.stack(200, 200, 4, 4, 0, 0, function(x,y) {
-    return Matter.Bodies.rectangle(x, y, 80, 80)
+let stack = Matter.Composites.stack((render.canvas.width / 2 - 80), 200, 8, 24, 0, 0, function(x,y) {
+    return Matter.Bodies.rectangle(x, y, 20, 20)
 });
 
 Matter.World.add(engine.world, [containerLeft, containerRight, containerDown, mouseConstraint, stack]);
